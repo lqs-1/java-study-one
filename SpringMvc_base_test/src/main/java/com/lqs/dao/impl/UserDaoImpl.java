f0ddde8291f1a746cab8f9c92ed948ddbcc00dfc
lqs <749062870@qq.com>
2022-01-07 20:41:10 +0800

    SpringMvc_base_test

--- SpringMvc_base_test/src/main/java/com/lqs/dao/impl/UserDaoImpl.java
+++ SpringMvc_base_test/src/main/java/com/lqs/dao/impl/UserDaoImpl.java
@@ -4,7 +4,12 @@ import com.lqs.dao.UserDao;
 import com.lqs.domain.User;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
@@ -25,6 +30,29 @@ public class UserDaoImpl implements UserDao {
     public int saveUser(String username, String password, String email, String phoneNum) {
         int rst = jdbcTemplate.update("insert into sys_user values (null,?,?,?,?)", username, password, email, phoneNum);
         return rst;
+
+        // 在添加中间表的数据的时候，需要user_id,也就是user表的主键，以下方法可以直接返回生成并且返回主键
+        // 创建PreparedStatementCreator， 用于执行sql
+//        PreparedStatementCreator preparedStatementCreator = new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+//                PreparedStatement preparedStatement = connection.prepareStatement("insert into sys_user values (?,?, ?, ?, ?)", PreparedStatement.RETURN_GENERATED_KEYS);
+//                preparedStatement.setObject(1, null);
+//                preparedStatement.setString(2, user.getUserName());
+//                preparedStatement.setString(3, user.getPasswrod);
+//                preparedStatement.setString(4, user.getEmail);
+//                preparedStatement.setString(5, user.getPhoneNum);
+//                return preparedStatement;
+//            }
+//        };
+
+        // 创建KeyHolder, 生成主键, 获取主键
+//        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+//        jdbcTemplate.update(preparedStatementCreator, generatedKeyHolder);
+        // 获取生成的主键
+//        int user_id = generatedKeyHolder.getKey().intValue();
+//        return user_id;
+
     }
 
     @Override
