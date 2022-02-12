fdeaad38a192ca1a95837284818ab6bed2ab43c6
lqs <749062870@qq.com>
2022-02-12 10:22:34 +0800

    1

--- spring-boot-config/src/main/java/com/lqs/SpringBootConfigApplication.java
+++ spring-boot-config/src/main/java/com/lqs/SpringBootConfigApplication.java
@@ -2,8 +2,10 @@ package com.lqs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+//@SpringBootApplication
 public class SpringBootConfigApplication {
 
     public static void main(String[] args) {
