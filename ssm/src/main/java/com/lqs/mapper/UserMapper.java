30c9b1246717ac5dd0eb847dc38086cb63355b5b
lqs <749062870@qq.com>
2022-02-27 19:47:10 +0800

    e

--- ssm/src/main/java/com/lqs/mapper/UserMapper.java
+++ ssm/src/main/java/com/lqs/mapper/UserMapper.java
@@ -1,9 +1,11 @@
 package com.lqs.mapper;
 
 import com.lqs.domain.User;
+//import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+//@Mapper
 public interface UserMapper {
     public void save(User user);
     public List<User> findAll();
